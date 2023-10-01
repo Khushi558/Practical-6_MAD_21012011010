29d2cb0da4270f32d95bbf86b7fe7bcfe9564c75
khushi <khushichaudhary21@gnu.ac.in>
2023-10-01 22:21:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practical_6_mad_21012011010/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.practical_6_mad_21012011010
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+class MainActivity : AppCompatActivity() {
+    var flag=1;
+    lateinit var playBtn:FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val stopBtn:FloatingActionButton = findViewById(R.id.stop)
+        playBtn = findViewById(R.id.play)
+        playBtn.setOnClickListener { playPauseMusic()}
+        stopBtn.setOnClickListener { stopMusic()}
+    }
+    fun playPauseMusic()
+    {
+        if(flag==1)
+        {
+            playBtn.setImageResource(R.drawable.baseline_pause_24)
+            flag=2
+        }
+        else
+        {
+
+            playBtn.setImageResource(R.drawable.baseline_play_arrow_24)
+            flag=1
+        }
+
+        Intent(applicationContext,MyService::class.java).putExtra(MyService. PLAYERKEY, MyService.PLAYERVALUE).apply {startService(this)}
+    }
+    fun stopMusic()
+    {
+        playBtn.setImageResource(R.drawable.baseline_play_arrow_24)
+        flag=1
+        Intent(applicationContext,MyService::class.java).putExtra(MyService. PLAYERKEY, MyService.PLAYERVALUE).apply {stopService(this)}
+    }
+}
